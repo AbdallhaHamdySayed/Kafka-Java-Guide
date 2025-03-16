3b92197be0b5473f34b24f2d1f2b1050b93c258c
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2025-03-16 12:45:12 +0200

    Create Kafka Consumer

--- src/main/java/net/javaguides/springboot/utils/AppConstants.java
+++ src/main/java/net/javaguides/springboot/utils/AppConstants.java
@@ -2,5 +2,5 @@ package net.javaguides.springboot.utils;
 
 public class AppConstants {
     public static final String TOPIC_NAME = "javaguides";
-//    public static final String GROUP_ID = "group_id";
+    public static final String GROUP_ID = "group_id";
 }
