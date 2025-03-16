43a954c8c5572b05189077a7087f05fbc869663a
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2025-03-17 01:51:27 +0200

    Create Kafka Consumer to Consume JSON Message

--- src/main/java/net/javaguides/springboot/kafka/KafKaConsumer.java
+++ src/main/java/net/javaguides/springboot/kafka/KafKaConsumer.java
@@ -1,5 +1,6 @@
 package net.javaguides.springboot.kafka;
 
+import net.javaguides.springboot.model.User;
 import net.javaguides.springboot.utils.AppConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,9 +12,17 @@ public class KafKaConsumer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(KafKaConsumer.class);
 
+    // String Message
+//    @KafkaListener(topics = AppConstants.TOPIC_NAME,
+//            groupId = AppConstants.GROUP_ID)
+//    public void consume(String message){
+//        LOGGER.info(String.format("Message received -> %s", message));
+//    }
+
+    // Json Message
     @KafkaListener(topics = AppConstants.TOPIC_NAME,
             groupId = AppConstants.GROUP_ID)
-    public void consume(String message){
-        LOGGER.info(String.format("Message received -> %s", message));
+    public void consume(User data){
+        LOGGER.info(String.format("Message received -> %s", data.toString()));
     }
 }
